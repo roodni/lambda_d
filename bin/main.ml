6fe8668b8d6a0e469c71aae4ba76253de0ff928b
rodoni <41773436+roodni@users.noreply.github.com>
2022-12-18 19:31:35 +0900

    3行以上の入力に対応

--- bin/main.ml
+++ bin/main.ml
@@ -32,17 +32,43 @@ let intaractive () =
 
 let fileloader path =
   let channel = open_in path in
-  let l1 = input_line channel in
-  let l2 = input_line channel in
-  let t1 = string_to_term l1 in
-  let t2 = string_to_term l2 in
-  if Judge.alpha_equal t1 t2 then begin
-    printf "alpha-equivalent\n";
-  end else begin
-    printf "NOT alpha-equivalent\n";
-  end;
-  print_newline ();
-  close_in channel
+  let rec lineloop eq_sets =
+    match input_line channel with
+    | exception End_of_file -> eq_sets
+    | line ->
+        let term = string_to_term line in
+        let cnt, eq_sets =
+          List.fold_left_map
+            (fun (cnt: int) (eq_set: Term.t list) ->
+              let res = List.map (Judge.alpha_equal term) eq_set in
+              if List.for_all Fun.id res then (* 同じだった *)
+                (cnt + 1, term :: eq_set)
+              else if List.for_all not res then (* 違った *)
+                (cnt, eq_set)
+              else (* 推移律に反する *)
+                assert false
+            )
+            0 eq_sets
+        in
+        ( match cnt with
+          | 0 -> lineloop ([term] :: eq_sets)
+          | 1 -> lineloop eq_sets
+          | _ -> assert false (* 推移律に反する *)
+        )
+  in
+  let eq_sets = lineloop [] in
+  List.iter
+    (fun eq_set ->
+      printf "{\n";
+      List.iter
+        (fun term ->
+          printf " ";
+          Term.print term;
+          printf "\n")
+        (List.rev eq_set);
+      printf "}\n";
+    )
+    (List.rev eq_sets)
 
 let () =
   if Array.length Sys.argv <= 1
